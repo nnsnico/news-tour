8c332b9fb4b7f77c9186011c328fa906fe63b665
nnsnico <yuji.jampan.blue@gmail.com>
2019-10-22 13:44:05 +0900

    Add news model

---
+++ src/news.rs
@@ -0,0 +1,48 @@
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+
+pub struct RequestNews {
+    pub url: String,
+    pub params: RequestParams,
+}
+
+#[derive(Debug, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct RequestParams {
+    pub country: String,
+    pub category: String,
+    pub page_size: usize,
+    pub api_key: String,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct ResponseNews {
+    pub status: String,
+    pub total_results: u32,
+    pub articles: Vec<Articles>,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Articles {
+    pub source: HashMap<String, Option<String>>,
+    pub author: Option<String>,
+    pub title: String,
+    pub description: String,
+    pub url: String,
+    pub url_to_image: Option<String>,
+    pub published_at: String,
+    pub content: Option<String>,
+}
+
+impl RequestNews {
+    pub fn get_topic(&self, endpoint: String) -> reqwest::Result<ResponseNews> {
+        let client = reqwest::Client::new();
+        let url = reqwest::Url::parse(&format!("{}{}", self.url, endpoint)).unwrap();
+
+        println!("{}", url);
+
+        client.get(url).query(&self.params).send()?.json()
+    }
+}
