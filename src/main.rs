8b659a6fe6e1572aafc2562a90dfffa20b6b1a6c
nnsnico <yuji.jampan.blue@gmail.com>
2020-05-17 21:35:38 +0900

    Fix to notify error

--- src/main.rs
+++ src/main.rs
@@ -4,32 +4,30 @@ use news_tour::settings::Settings;
 use news_tour::slack::*;
 use std::env;
 
-fn main() {
-    let settings = Settings::create_new().unwrap();
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let settings = Settings::create_new()?;
 
     let news_params = RequestParams::new(
         settings.api.country,
         settings.api.category,
         settings.api.page_size,
-        env::var(&settings.api.api_key).unwrap(),
+        env::var(&settings.api.api_key)?,
     );
     let news = RequestNews::new(settings.api.scheme, settings.api.domain, news_params);
 
-    let news_response: ResponseNews = news.get_topic(settings.api.endpoint).unwrap();
+    let news_response: ResponseNews = news.get_topic(settings.api.endpoint)?;
     let filtered_response: ResponseNews =
         news_response.filter_by_source(settings.api.exclude_news_domain);
 
     let slack_params = SlackParams::new(
-        env::var(&settings.slack.token_key).unwrap(),
+        env::var(&settings.slack.token_key)?,
         filtered_response.create_summary(),
         settings.bot.channel,
         settings.bot.as_user,
     );
     let slack = Slack::new(settings.slack.scheme, settings.slack.domain, slack_params);
 
-    let slack_response = slack.post_message(settings.slack.post_message_endpoint);
+    slack.post_message(settings.slack.post_message_endpoint)?;
 
-    if let Err(err) = slack_response {
-        println!("Failed: {}", err);
-    }
+    Ok(())
 }
